cc01b3ce2d5aed842631fa6cd593a9b7ca72d639
PAVOL SLOVAK <pavol.slovak1995@gmail.com>
2025-01-13 09:32:19 +1100

    update

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  envDir: ".", // Ensure Vite looks for .env in the root directory
+
   server: {
     fs: {
       strict: false,
