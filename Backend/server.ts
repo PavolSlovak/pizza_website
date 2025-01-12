1f938e96ccb0672d316796b1ea15b3684efff2a7
PAVOL SLOVAK <pavol.slovak1995@gmail.com>
2025-01-12 16:52:59 +1100

    update

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,4 +1,3 @@
-// server.js
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
 import { config as dotenvConfig } from "dotenv";
@@ -8,14 +7,23 @@ import path from "path";
 
 dotenv.config();
 const app = express();
-
-// Remove app.listen() for serverless
-connectDB();
-app.use(corsMiddleware);
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
+
+console.log("Port:", process.env.PORT);
+console.log("Environment:", process.env.NODE_ENV);
+
+if (process.env.NODE_ENV !== "production") {
+  dotenvConfig();
+}
+
+connectDB();
+
+app.use(corsMiddleware);
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 import Stripe from "stripe";
 if (!process.env.STRIPE_PRIVATE_KEY) {
@@ -30,15 +38,21 @@ import { locationRouter } from "./routes/locationRoutes.js";
 import { subscribeRouter } from "./routes/subscribeRoutes.js";
 
 app.use("/api/orders", orderRouter);
+
 app.use("/api/menus", menuRouter);
+
 app.use("/api/locations", locationRouter);
+
 app.use("/api/subscribe", subscribeRouter);
 
 // Handle root route
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, welcome to the API!");
 });
-// Export the handler to Vercel
-export default (req: Request, res: Response) => {
-  app(req, res); // Use Express as a handler for serverless
-};
+
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+export default app;
