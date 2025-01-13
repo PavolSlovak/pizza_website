ebc2be4bae79bf9f735378e09faa01173523b5ff
PAVOL SLOVAK <pavol.slovak1995@gmail.com>
2025-01-13 14:33:35 +1100

    update

--- Backend/dist/middlewares/cors.js
+++ Backend/dist/middlewares/cors.js
@@ -6,9 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.corsMiddleware = void 0;
 const cors_1 = __importDefault(require("cors"));
 const corsOptions = {
-    origin: process.env.NODE_ENV === "production" ? process.env.CLIENT_URL : "*", // Use CLIENT_URL for production, allow all origins in development
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // If your requests include credentials (cookies, HTTP auth)
 };
 console.log("Environment:", process.env.NODE_ENV); // Check the environment
 console.log("Client URL:", process.env.CLIENT_URL); // Check the client URL
